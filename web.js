36167e865a1f30ff26990e7b7052bab671effd26
Ubuntu <ubuntu@ip-172-31-18-195.ap-southeast-1.compute.internal>
2013-07-19 20:50:26 +0000

    include fs, buffer

--- web.js
+++ web.js
@@ -2,8 +2,13 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var buffer = require('buffer');
+var fs = require('fs');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var data = fs.readFileSync('./bitstarter/index.html');
+    var buf = new Buffer(data.length);
+    response.send(buf.toString('utf8', 0, data.length));
 });
 
 var port = process.env.PORT || 5000;
