be320667b4c446d6a98c93e61e634d00f6e3e5a8
Ubuntu <ubuntu@ip-172-31-18-195.ap-southeast-1.compute.internal>
2013-07-19 21:03:46 +0000

    finally works.

--- web.js
+++ web.js
@@ -2,14 +2,11 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
-var buffer = require('buffer');
 var fs = require('fs');
 
 app.get('/', function(request, response) {
     var data = fs.readFileSync('index.html');
-    var buf = new Buffer(data.length);
-	buf.write(data);
-    response.send(buf.toString('utf8', 0, data.length));
+    response.send(data.toString('utf8', 0, data.length));
 });
 
 var port = process.env.PORT || 5000;
